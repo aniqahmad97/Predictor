11af15710069aeb5bb81819300a450a3ae3fd30d
Aniq Ahmad <Aniq.Ahmad-1@ou.edu>
2026-01-18 14:26:14 -0600

    Added all files

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import numpy as np
+import pickle
+st.title("Income Predictor")
+
+
+st.set_page_config(page_title='Income Predictor')
+
+# st.camera_input(label='camera')
+# st.button(label='Click Here')
+# if st.button("Ballons"):
+#     st.ballons()
+# st.write("I dont what to do ")
+# st.error("This is errors")
+# Insert a chat message container.
+with open("model.pkl",'rb') as file:
+    model=pickle.load(file) 
+
+input=st.number_input(label="Enter Experience in years",min_value=0.0,max_value=10.0,value=0.5,step=0.1) 
+
+
+
+if st.button("Predict"):
+    predictions=model.predict([[input]])[0]
+    st.success(predictions)  
+    st.balloons()  
+
